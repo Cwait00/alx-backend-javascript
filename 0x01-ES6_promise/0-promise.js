20ade026caeab3353f2f5d7a963f7047c300f4e6
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-24 14:29:35 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API request with a 2-second delay
-    setTimeout(() => {
-      // Resolve the Promise with a sample response
-      resolve({ status: 200, data: 'Success' });
-    }, 2000);
-  });
+  return Promise.resolve(true);
 }
