8c0a682a9892c01fe46a24a3c71059f6b8280199
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-24 13:55:57 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,9 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation (e.g., fetching data from an API)
+    // Simulate an API request with a 2-second delay
     setTimeout(() => {
-      // Resolve the Promise with some sample response data
-      resolve("Sample API response data");
-    }, 1000); // Simulating a delay of 1 second
+      // Resolve the Promise with a sample response
+      resolve({ status: 200, data: 'Success' });
+    }, 2000);
   });
 }
-
-export default getResponseFromAPI;
