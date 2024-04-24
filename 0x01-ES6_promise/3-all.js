b7f07e206f649c4c99eeaf27509ccc64a5d3162c
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-24 18:40:01 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
-      if (photo && photo.body && user && user.firstName && user.lastName) {
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-      }
-    })
+async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.log('Signup system offline');
+  }
 }
 
 export default handleProfileSignup;
