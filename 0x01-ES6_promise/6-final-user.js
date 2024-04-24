07dd81bc4c1cf6d5097e42a2a9dbf3b8187f873c
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-24 19:32:09 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call signUpUser and uploadPhoto functions with provided arguments
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ];
 
-  // Wait for both promises to settle
-  return Promise.allSettled([promise1, promise2]).then(results => {
-    // Construct array containing objects with status and value or error
-    return results.map(result => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    }));
-  });
+  return Promise.allSettled(promises);
 }
