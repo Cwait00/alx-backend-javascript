6cf76887c64073da7dad507fff6ef7c2e707cf50
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-25 23:12:03 +0000

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,21 +1,11 @@
-const cloneMethod = Symbol('clone');
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  [cloneMethod]() {
-    const Constructor = this.constructor; // Get the constructor function of the current instance
-    const { _brand, _motor, _color } = this;
-    return new Constructor(_brand, _motor, _color); // Create a new instance using the constructor function
-  }
-
   cloneCar() {
-    return this[cloneMethod]();
+    return new Car(this._brand, this._motor, this._color);
   }
 }
-
-export default Car;
