d56e19641aa2ebac6cf3087eca2b7f465d5d6e1e
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-30 16:13:44 +0000

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,14 @@
+// 5-typed_arrays.js
 function createInt8TypedArray(length, position, value) {
-    if (position >= length || position < 0) {
-        throw new Error('Position outside range');
-    }
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
-    int8Array[position] = value;
+  let buffer = new ArrayBuffer(length);
+  let int8View = new Int8Array(buffer);
+  int8View[position] = value;
 
-    return new DataView(buffer);
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
