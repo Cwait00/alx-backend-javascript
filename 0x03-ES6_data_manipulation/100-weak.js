0132bb18e1c08625a2d5f2fec5d0ac70593f68bc
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-30 17:00:52 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,16 +1,15 @@
+// 100-weak.js
 const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
-    if (!weakMap.has(endpoint)) {
-        weakMap.set(endpoint, 0);
-    }
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
 
-    const count = weakMap.get(endpoint) + 1;
-    weakMap.set(endpoint, count);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
 
-    if (count >= 5) {
-        throw new Error('Endpoint load is high');
-    }
+  weakMap.set(endpoint, count);
 }
 
-export { weakMap, queryAPI };
+export { queryAPI, weakMap };
