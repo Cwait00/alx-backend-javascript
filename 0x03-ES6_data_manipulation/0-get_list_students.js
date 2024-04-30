d134501e83cbd247c5e593605339e511b3c63a87
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-30 15:30:47 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,9 @@
 function getListStudents() {
-    const students = [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
-    return students;
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
 
 export default getListStudents;
