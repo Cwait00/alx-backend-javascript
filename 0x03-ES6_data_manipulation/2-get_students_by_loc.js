1ba666866bf2f2c15448356982a58170105581c7
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-30 15:55:41 +0000

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,9 @@
-function getStudentsByLocation(students, city) {
-    return students.filter(student => student.location === city);
-}
+// 2-get_students_by_loc.js
+const getStudentsByLocation = (students, city) => {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
+};
 
 export default getStudentsByLocation;
