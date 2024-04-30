754d8010b74f6adbf1a7ccb25b26635fe44636eb
Cwait00 <cwaitanobongoza@gmail.com>
2024-04-30 16:33:50 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,13 @@
+// 8-clean_set.js
 function cleanSet(set, startString) {
-    const filteredValues = [...set].filter(value => value.startsWith(startString));
-    return filteredValues.map(value => value.substring(startString.length)).join('-');
+  if (typeof startString !== 'string' || startString === '') {
+    return '';
+  }
+
+  return [...set]
+    .filter(item => typeof item === 'string' && item.startsWith(startString))
+    .map(item => item.slice(startString.length))
+    .join('-');
 }
 
 export default cleanSet;
