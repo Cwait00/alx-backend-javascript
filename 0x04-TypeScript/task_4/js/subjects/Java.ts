d59492d3e1596e22669f9de7ad879e254e97f452
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-02 18:38:13 +0000

    task_4/js/subjects/Java.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,21 +1,19 @@
-// Java.ts
-
 namespace Subjects {
-    export interface Teacher {
-        experienceTeachingJava?: number;
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Java";
     }
 
-    export class Java extends Subject {
-        getRequirements(): string {
-            return "Here is the list of requirements for Java";
-        }
-
-        getAvailableTeacher(): string {
-            if (this.teacher && this.teacher.experienceTeachingJava !== undefined) {
-                return `Available Teacher: ${this.teacher.firstName}`;
-            } else {
-                return "No available teacher";
-            }
-        }
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava !== undefined) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
     }
+  }
 }
