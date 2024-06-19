d36ec55ee60eecd8b9dce2312c699aa634d9ae7b
cwait00 <cwaitanobongoza@gmail.com>
2024-06-19 16:32:08 +0000

    2-calcul_chai.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,46 @@
+// 2-calcul_chai.test.js
+
+const expect = require('chai').expect;
+const calculateNumber = require('./2-calcul_chai.js');
+
+describe('calculateNumber', function() {
+  describe('SUM', function() {
+    it('should return 6 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+    });
+
+    it('should return 5 when inputs are 1.2 and 3.7', function() {
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    });
+  });
+
+  describe('SUBTRACT', function() {
+    it('should return -4 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
+
+    it('should return -3 when inputs are 1.2 and 3.7', function() {
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+    });
+  });
+
+  describe('DIVIDE', function() {
+    it('should return 0.2 when inputs are 1.4 and 4.5', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+
+    it('should return "Error" when dividing by 0', function() {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
+
+    it('should return approximately 2.333 when inputs are 6.7 and 3.2', function() {
+      expect(calculateNumber('DIVIDE', 6.7, 3.2)).to.be.closeTo(2.333, 0.001);
+    });
+  });
+
+  describe('Invalid type', function() {
+    it('should throw an error when type is invalid', function() {
+      expect(() => calculateNumber('INVALID', 1.4, 4.5)).to.throw(Error, 'Invalid type');
+    });
+  });
+});
