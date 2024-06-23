63b8e1829ff7a2b696697b6b6efc1ccf60ca57d3
cwait00 <cwaitanobongoza@gmail.com>
2024-06-23 19:26:23 +0000

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
-    process.exit();
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  process.exit();
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
