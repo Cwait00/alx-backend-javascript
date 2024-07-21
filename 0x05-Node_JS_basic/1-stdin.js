9dbb0a74ecca685c9fcada623f2bb8f4a8ba43c0
cwait00 <cwaitanobongoza@gmail.com>
2024-07-21 16:47:46 +0000

    implement 1-stdin.js to read user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit(); // Exit the program after processing input
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdin.pause(); // Pause the input stream instead of ending it
 });
 
 process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
+
+process.stdin.resume();
